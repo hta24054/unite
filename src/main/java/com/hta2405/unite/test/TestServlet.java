2f9254f19f83afd5f40017f0f827bc3baca2a9c1
DNGHKM <kdh940@gmail.com>
2024-10-30 16:08:13 +0900

    doc 파일 추가

--- src/main/java/com/hta2405/unite/test/TestServlet.java
+++ src/main/java/com/hta2405/unite/test/TestServlet.java
@@ -13,9 +13,11 @@ import java.io.IOException;
 public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        TestClass member = new TestClass("홍길동", 20);
+        TestClass member1 = new TestClass("이순신", 50);
+        TestClass member2 = new TestClass("홍길동", 20);
         RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
-        req.setAttribute("member", member);
+        req.setAttribute("member1", member1);
+        req.setAttribute("member2", member2);
         dispatcher.forward(req, resp);
     }
 }

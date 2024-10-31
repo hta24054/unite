c42799e38820af5f7b811c970d0b435dbbe8cff6
DNGHKM <kdh940@gmail.com>
2024-10-31 12:27:20 +0900

    dto 작성, 휴가 받아오기 기능 구현

---
+++ src/main/java/com/hta2405/unite/dto/Sign.java
@@ -0,0 +1,15 @@
+package com.hta2405.unite.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Sign {
+    private Long signId;
+    private String EmpId;
+    private Long docId;
+    private Long signOrder;
+}

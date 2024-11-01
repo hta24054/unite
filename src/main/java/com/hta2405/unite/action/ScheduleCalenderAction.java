348782a0f25f569aae2315e626c8d5f5745818ab
choijiiyoung <blueberryxchoco@gmail.com>
2024-11-01 17:45:29 +0900

    setPath 롤백

--- src/main/java/com/hta2405/unite/action/ScheduleCalenderAction.java
+++ src/main/java/com/hta2405/unite/action/ScheduleCalenderAction.java
@@ -13,19 +13,9 @@ public class ScheduleCalenderAction implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		int scheduleId = Integer.parseInt(request.getParameter("schedule_id"));
-		String empId = request.getParameter("emp_id");
-		String schedule_name = request.getParameter("schedule_name");
-		String schedule_start = request.getParameter("schedule_start");
-		String schedule_end = request.getParameter("schedule_end");
-		//bgColor값 추가 필요
-		
-		Schedule schedule = new Schedule();
-		
-		
 		ActionForward forward = new ActionForward();
 		forward.setRedirect(false); //포워딩 방식으로 주소가 바뀌지 않아요
-		forward.setPath("/WEB-INF/views/calender/calenderSchedule.jsp");
+		forward.setPath("/WEB-INF/views/schedule/scheduleCalender.jsp");
 		
 		return forward;
 	}

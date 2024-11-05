829750e57dab5639123319236389fcadb36a176c
DNGHKM <kdh940@gmail.com>
2024-11-05 13:27:32 +0900

    1. 하위 부서 직원 목록 반환 메서드 이름 수정 및 해당 메서드 admin 호출 시 전체 직원 가져오도록 수정

--- src/main/java/com/hta2405/unite/action/AttendEmpListAction.java
+++ src/main/java/com/hta2405/unite/action/AttendEmpListAction.java
@@ -19,7 +19,6 @@ public class AttendEmpListAction implements Action {
         String empId = (String) req.getSession().getAttribute("id");
         EmpDao empDao = new EmpDao();
         Emp emp = empDao.getEmpById(empId);
-
         DeptDao deptDao = new DeptDao();
         if (!isManager(emp)) {
             //부서장이 아닐 시 권한없음 알림 및 에러페이지로 포워딩
@@ -29,7 +28,7 @@ public class AttendEmpListAction implements Action {
             out.print("history.back()</script>");
             return null;
         }
-        List<Emp> empList = empDao.getEmpListByDeptId(emp);
+        List<Emp> empList = empDao.getSubEmpListByEmp(emp);
         req.setAttribute("empList", empList);
         req.setAttribute("deptName", getDeptName(emp.getDeptId(), deptDao));
         return new ActionForward(false, "/WEB-INF/views/attend/attendEmpList.jsp");

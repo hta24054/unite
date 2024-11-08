98ca9cae4db3ca90c8c5e5683d75c7e06b7173d8
choijiiyoung <blueberryxchoco@gmail.com>
2024-11-08 15:52:17 +0900

    js에서 로드하면서 value="${id}"값을 불러오므로 캘린더 메인에서 id값 가져올 필요 없어짐

--- src/main/java/com/hta2405/unite/action/ScheduleCalenderAction.java
+++ src/main/java/com/hta2405/unite/action/ScheduleCalenderAction.java
@@ -2,9 +2,6 @@ package com.hta2405.unite.action;
 
 import java.io.IOException;
 
-import com.google.gson.JsonArray;
-import com.hta2405.unite.dao.ScheduleDAO;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,23 +10,6 @@ public class ScheduleCalenderAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		// 세션에서 로그인한 id값을 getAttribute로 가져온다.
-		String id = (String) request.getSession().getAttribute("id");
-		
-		ScheduleDAO sdao = new ScheduleDAO();
-        
-		JsonArray array = sdao.getSchedule(id);
-
-        response.setContentType("application/json;charset=utf-8");
-
-        if (array != null) {
-            response.getWriter().print(array);
-        } else {
-            response.getWriter().print("[]"); 
-        }
-        
-        System.out.println(array); 
 
         ActionForward forward = new ActionForward();
         forward.setRedirect(false); 

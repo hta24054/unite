4281d433303f66cea49144b7541767497b8ddbf5
choijiiyoung <blueberryxchoco@gmail.com>
2024-11-20 17:49:00 +0900

    GetMyReservationDetailAction  임시

--- src/main/java/com/hta2405/unite/action/reservation/GetMyReservationDetailAction.java
+++ src/main/java/com/hta2405/unite/action/reservation/GetMyReservationDetailAction.java
@@ -1,7 +1,32 @@
 package com.hta2405.unite.action.reservation;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import com.hta2405.unite.action.Action;
+import com.hta2405.unite.action.ActionForward;
+import com.hta2405.unite.dao.ReservationDAO;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class GetMyReservationDetailAction implements Action {
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+		String empId = request.getParameter("emp_id"); // 예약자
+	
+		ReservationDAO reservationDAO = new ReservationDAO();
+        List<Map<String, Object>> reservationList = reservationDAO.getMyReservationList(empId);
+        
+        ActionForward forward = new ActionForward();
+		forward.setPath("/WEB-INF/views/reservation/myReservationList.jsp");
+		forward.setRedirect(false);
+        request.setAttribute("reservationList", reservationList);
+        
+        return forward;
+	}
 }

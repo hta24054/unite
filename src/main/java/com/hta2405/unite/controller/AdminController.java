c88f120772e85b1bdbaf8922bf12f517e80a37e3
DNGHKM <kdh940@gmail.com>
2024-12-31 11:20:18 +0900

    [ADD] 관리자 - 휴일관리 - 휴일 조회 구현

--- src/main/java/com/hta2405/unite/controller/AdminController.java
+++ src/main/java/com/hta2405/unite/controller/AdminController.java
@@ -1,11 +1,45 @@
 package com.hta2405.unite.controller;
 
+import com.hta2405.unite.domain.Holiday;
+import com.hta2405.unite.service.AdminService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 @Slf4j
 public class AdminController {
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    //              url: "../admin/holiday/list?start=" + startMonth + "&end=" + endMonth,
+    @GetMapping("/holiday")
+    public String showHolidayPage() {
+        return "/admin/holiday";
+    }
+
+
+    @GetMapping("/holiday/list")
+    @ResponseBody
+    public List<Holiday> getHolidayList(String start, String end, ModelAndView mv) {
+        String[] startString = start.split("-");
+        String[] endString = end.split("-");
+
+        LocalDate startDate = LocalDate.of(Integer.parseInt(startString[0]), Integer.parseInt(startString[1]), 1);
+        YearMonth endYearMonth = YearMonth.of(Integer.parseInt(endString[0]), Integer.parseInt(endString[1]));
+        LocalDate endDate = endYearMonth.atEndOfMonth();
+
+       return adminService.getHolidayList(startDate, endDate);
+    }
 }
---
+++ src/main/java/com/hta2405/unite/mybatis/mapper/HolidayMapper.java
@@ -0,0 +1,21 @@
+package com.hta2405.unite.mybatis.mapper;
+
+import com.hta2405.unite.domain.Holiday;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Mapper
+public interface HolidayMapper {
+    int insertHoliday(Holiday holiday);
+
+    String getHolidayName(LocalDate date);
+
+    int updateHoliday(Holiday holiday);
+
+    List<Holiday> getHolidayList(@Param("startDate") LocalDate statDate, @Param("endDate") LocalDate endDate);
+
+    int deleteHoliday(LocalDate date);
+}

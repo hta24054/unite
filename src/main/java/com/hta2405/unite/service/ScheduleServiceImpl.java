088a47b948b8bc3612f13dac314842251e9eccc6
choijiiyoung <blueberryxchoco@gmail.com>
2024-12-31 17:43:45 +0900

    Schedule 캘린더 변환 작업 중

---
+++ src/main/java/com/hta2405/unite/service/ScheduleServiceImpl.java
@@ -0,0 +1,26 @@
+package com.hta2405.unite.service;
+
+import com.hta2405.unite.domain.Schedule;
+import com.hta2405.unite.mybatis.mapper.ScheduleMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ScheduleServiceImpl implements ScheduleService {
+
+    private ScheduleMapper dao;
+    public ScheduleServiceImpl(ScheduleMapper schedule) {
+        this.dao = schedule;
+    }
+
+    @Override
+    public List<Schedule> getListSchedule(String id) {
+        return dao.getListSchedule(id);
+    }
+
+    @Override
+    public int insertSchedule(Schedule schedule) {
+        return dao.insertSchedule(schedule);
+    }
+}

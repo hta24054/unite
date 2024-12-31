756fab79e075c0a6536fe0e04facbc59de698309
DNGHKM <kdh940@gmail.com>
2024-12-31 17:47:57 +0900

    [ADD] 인사정보 조회 구현

--- src/main/java/com/hta2405/unite/service/EmpService.java
+++ src/main/java/com/hta2405/unite/service/EmpService.java
@@ -1,11 +1,16 @@
 package com.hta2405.unite.service;
 
+import com.hta2405.unite.domain.Cert;
 import com.hta2405.unite.domain.Emp;
-import com.hta2405.unite.mybatis.mapper.EmpMapper;
+import com.hta2405.unite.domain.Lang;
+import com.hta2405.unite.dto.EmpInfoDTO;
+import com.hta2405.unite.mybatis.mapper.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,8 +18,27 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class EmpService {
     private final EmpMapper empMapper;
+    private final LangMapper langMapper;
+    private final CertMapper certMapper;
+    private final JobMapper jobMapper;
+    private final DeptMapper deptMapper;
 
     public Optional<Emp> getEmpById(String empId) {
         return empMapper.getEmpById(empId);
+    }
+
+    public EmpInfoDTO getEmpInfoDTO(String empId) {
+        Emp emp = getEmpById(empId).orElseThrow(
+                () -> new UsernameNotFoundException("유저를 찾을 수 없습니다."));
+        String deptName = deptMapper.getDeptByEmpId(empId).getDeptName();
+        String jobName = jobMapper.getJobByEmpId(empId).getJobName();
+        List<Lang> langList = langMapper.getLangByEmpId(empId);
+        List<Cert> certList = certMapper.getCertByEmpId(empId);
+        return EmpInfoDTO.builder()
+                .emp(emp)
+                .deptName(deptName)
+                .jobName(jobName)
+                .langList(langList)
+                .certList(certList).build();
     }
 }

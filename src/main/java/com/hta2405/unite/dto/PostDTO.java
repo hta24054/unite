7a7228e31fe307b9465f674de9abf5f684b95c95
HuYeoun <kangyh5560@naver.com>
2025-01-02 18:07:44 +0900

    게시판 홈, 글쓰기 일부 구현

--- src/main/java/com/hta2405/unite/dto/PostDTO.java
+++ src/main/java/com/hta2405/unite/dto/PostDTO.java
@@ -3,9 +3,6 @@ package com.hta2405.unite.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter

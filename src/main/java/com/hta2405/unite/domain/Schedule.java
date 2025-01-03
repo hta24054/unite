44d2cfaa7595f15859fe55a6a63989dc6422f084
choijiiyoung <blueberryxchoco@gmail.com>
2025-01-03 11:43:03 +0900

    @DateTimeFormat 어노테이션 추가

--- src/main/java/com/hta2405/unite/domain/Schedule.java
+++ src/main/java/com/hta2405/unite/domain/Schedule.java
@@ -1,6 +1,7 @@
 package com.hta2405.unite.domain;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -10,7 +11,9 @@ public class Schedule {
     private String empId;			//일정등록자
     private String scheduleName;	//일정명
     private String scheduleContent; //일정내용
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime scheduleStart;//시작일시
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime scheduleEnd;//종료일시
     private String scheduleColor;    //일정색상
     private boolean scheduleAllDay;		//종일

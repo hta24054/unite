01e73dd51ad5a3777720fd6b5c93a464de014911
DNGHKM <kdh940@gmail.com>
2025-01-03 14:49:54 +0900

    [ADD] 조직도 구현

--- src/main/java/com/hta2405/unite/mybatis/mapper/EmpMapper.java
+++ src/main/java/com/hta2405/unite/mybatis/mapper/EmpMapper.java
@@ -1,8 +1,10 @@
 package com.hta2405.unite.mybatis.mapper;
 
 import com.hta2405.unite.domain.Emp;
+import com.hta2405.unite.dto.EmpTreeDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -12,4 +14,8 @@ public interface EmpMapper {
     int update(Emp emp);
 
     String getImgOriginal(String fileUUID);
+
+    List<EmpTreeDTO> getEmpListByDeptId(Long deptId);
+
+    List<EmpTreeDTO> getEmpListByName(String ename);
 }

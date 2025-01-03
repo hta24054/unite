41075d62bc432692e2ab8890363943ea8cf1b33f
DNGHKM <kdh940@gmail.com>
2025-01-03 10:59:23 +0900

    [ADD] 인사정보 수정 [ADD] 프로필사진 업로드, 다운로드

--- src/main/java/com/hta2405/unite/mybatis/mapper/CertMapper.java
+++ src/main/java/com/hta2405/unite/mybatis/mapper/CertMapper.java
@@ -2,10 +2,15 @@ package com.hta2405.unite.mybatis.mapper;
 
 import com.hta2405.unite.domain.Cert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface CertMapper {
     List<Cert> getCertByEmpId(String empId);
+
+    void deleteAllCertByEmpId(String empId);
+
+    void insertCert(@Param("list") List<Cert> certList);
 }
--- src/main/java/com/hta2405/unite/mybatis/mapper/JobMapper.java
+++ src/main/java/com/hta2405/unite/mybatis/mapper/JobMapper.java
@@ -3,7 +3,11 @@ package com.hta2405.unite.mybatis.mapper;
 import com.hta2405.unite.domain.Job;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface JobMapper {
     Job getJobByEmpId(String empId);
+
+    List<Job> getAllJob();
 }

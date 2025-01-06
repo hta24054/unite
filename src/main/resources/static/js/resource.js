7b94a6a26b6f0a866b02521f4bd434957afa6e90
DNGHKM <kdh940@gmail.com>
2025-01-06 17:26:24 +0900

    [UPDATE] resource 페이지 템플릿 변경, 테이블 레이아웃 한글화

--- src/main/resources/static/js/resource.js
+++ src/main/resources/static/js/resource.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
     const contextPath = /*[[@{}]]*/ '';
+    const lang_kor = {
+        "decimal": "",
+        "emptyTable": "데이터가 없습니다.",
+        "info": "_START_-_END_ (총 _TOTAL_명)",
+        "infoEmpty": "0명",
+        "infoFiltered": "(전체 _MAX_명 중 검색 결과)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "_MENU_개씩 보기",
+        "loadingRecords": "로딩중...",
+        "processing": "처리중...",
+        "search": "검색:",
+        "zeroRecords": "검색된 데이터가 없습니다.",
+        "paginate": {"first": "첫 페이지", "last": "마지막 페이지", "next": "다음", "previous": "이전"},
+        "aria": {"sortAscending": ":오름차순 정렬", "sortDescending": ":내림차순 정렬"}
+    };
 
     // DataTable 초기화
     const resourceTable = $('#resourceTable').DataTable({
+        language: lang_kor,
         ajax: {
             url: contextPath + "/admin/resource/list",
-            dataSrc: 'data'
+            dataSrc: 'data',
+
         },
         columns: [
             {
@@ -100,10 +118,11 @@ $(document).ready(function () {
     document.querySelectorAll('button, input, select, textarea').forEach(element => {
         element.blur();
     });
-    $('#editClose').click(function (){
+    $('#editClose').click(function () {
         console.log("눌렸다")
     })
 });
+
 // 수정 모달 열기
 function openEditModal() {
     const selectedCheckbox = $('.checkbox:checked');

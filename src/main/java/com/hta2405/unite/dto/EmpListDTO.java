0a24b997197b31bfff5d3358fba6dad688db5cc9
DNGHKM <kdh940@gmail.com>
2025-01-06 14:12:42 +0900

    [ADD] 직원 등록, 삭제 구현 [ADD] 하위 부서 조회

---
+++ src/main/java/com/hta2405/unite/dto/EmpListDTO.java
@@ -0,0 +1,14 @@
+package com.hta2405.unite.dto;
+
+import lombok.*;
+
+@Data
+public class EmpListDTO {
+    private String empId;
+    private String ename;
+    private String deptName;
+    private String jobName;
+    private String email;
+    private String tel;
+    private String mobile;
+}

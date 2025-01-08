e5d0df1b4ef2ed140dd942f1befe1d5755c626c5
DNGHKM <kdh940@gmail.com>
2025-01-08 12:34:33 +0900

    [ADD] 출장신청서 작성(저장) 구현

--- src/main/java/com/hta2405/unite/strategy/TripDocSaver.java
+++ src/main/java/com/hta2405/unite/strategy/TripDocSaver.java
@@ -1,12 +1,21 @@
 package com.hta2405.unite.strategy;
 
+import com.hta2405.unite.domain.Doc;
+import com.hta2405.unite.domain.DocTrip;
 import com.hta2405.unite.dto.DocSaveRequestDTO;
+import com.hta2405.unite.enums.DocType;
+import com.hta2405.unite.service.DocService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class TripDocSaver implements DocSaver {
+    private final DocService docService;
 
     @Override
     public String getType() {
@@ -14,7 +23,27 @@ public class TripDocSaver implements DocSaver {
     }
 
     @Override
-    public void save(String empId, DocSaveRequestDTO docSaveRequestDTO) {
+    public void save(String empId, DocSaveRequestDTO req) {
+        Map<String, Object> formData = req.getFormData();
+        System.out.println(req.getFormData());
+        Doc doc = Doc.builder().docWriter(empId)
+                .docType(DocType.TRIP)
+                .docTitle("출장신청서(" + formData.get("writer").toString() + ")")
+                .docContent(formData.get("trip_info").toString())
+                .docCreateDate(LocalDateTime.now())
+                .signFinish(false).build();
 
+        DocTrip docTrip = DocTrip.builder()
+                .tripStart(LocalDate.parse(formData.get("trip_start").toString()))
+                .tripEnd(LocalDate.parse(formData.get("trip_end").toString()))
+                .tripLoc(formData.get("trip_loc").toString())
+                .tripPhone(formData.get("trip_phone").toString())
+                .tripInfo(formData.get("trip_info").toString())
+                .cardStart(LocalDate.parse(formData.get("card_start").toString()))
+                .cardEnd(LocalDate.parse(formData.get("card_end").toString()))
+                .cardReturn(LocalDate.parse(formData.get("card_return").toString()))
+                .build();
+
+        docService.saveTripDoc(doc, docTrip, req.getSigners());
     }
 }

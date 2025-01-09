396f09596a37ef90e1beebc047a4a3ee6b9094b2
choijiiyoung <blueberryxchoco@gmail.com>
2025-01-09 09:49:42 +0900

    일정등록/공유일정등록 - 소스 복구

--- src/main/java/com/hta2405/unite/domain/ScheduleShare.java
+++ src/main/java/com/hta2405/unite/domain/ScheduleShare.java
@@ -6,5 +6,5 @@ import lombok.Data;
 public class ScheduleShare {
     private int scheduleShareId;    //일정공유ID
     private String shareEmp;        //공유직원
-    private int scheduleId;            //일정코드
+    private int scheduleId;         //일정코드
 }

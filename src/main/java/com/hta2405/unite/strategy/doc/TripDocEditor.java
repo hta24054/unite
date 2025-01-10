7129a1116ba8d78220d271b829ee23d8b666e6e7
DNGHKM <kdh940@gmail.com>
2025-01-10 12:51:28 +0900

    [FIX] 클래스명 오타 수정

--- src/main/java/com/hta2405/unite/strategy/doc/TripDocEditor.java
+++ src/main/java/com/hta2405/unite/strategy/doc/TripDocEditor.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
-public class TipDocEditor implements DocEditor {
+public class TripDocEditor implements DocEditor {
 
     private final DocService docService;
 

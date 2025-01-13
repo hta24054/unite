b2e6e616953ed52d85d57660ed9dc8c21956f02b
DNGHKM <kdh940@gmail.com>
2025-01-13 14:35:00 +0900

    [ADD] 날씨정보 추가

--- src/main/resources/static/js/layout.js
+++ src/main/resources/static/js/layout.js
@@ -7,4 +7,25 @@ $(document).ready(function () {
             localStorage.setItem('sb|sidebar-toggle', $('body').hasClass('sb-sidenav-toggled'));
         });
     }
+    // 날씨정보
+    function updateWeatherUI(weather) {
+        const iconUrl = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+        $('#weatherIcon').attr('src', iconUrl);
+        $('#currentTemp').text(weather.main.temp.toFixed(1));
+        $('#maxTemp').text(weather.main.temp_max.toFixed(1));
+        $('#minTemp').text(weather.main.temp_min.toFixed(1));
+    }
+
+    function fetchWeather() {
+        $.get('/api/weather')
+            .done(function (data) {
+                updateWeatherUI(data);
+            })
+            .fail(function () {
+                console.error('Failed to fetch weather data.');
+            });
+    }
+
+    // 페이지 로드 시 날씨 정보 가져오기
+    fetchWeather();
 });

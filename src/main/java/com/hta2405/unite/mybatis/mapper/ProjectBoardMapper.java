50afb853a5796479dd97b6fbc7e270d338bda069
dontotl18 <dbsgksthf101@naver.com>
2025-01-14 09:09:32 +0900

    부트스트랩 4 -> 5

--- src/main/java/com/hta2405/unite/mybatis/mapper/ProjectBoardMapper.java
+++ src/main/java/com/hta2405/unite/mybatis/mapper/ProjectBoardMapper.java
@@ -4,13 +4,11 @@ import com.hta2405.unite.domain.ProjectTask;
 import com.hta2405.unite.dto.FileDTO;
 import com.hta2405.unite.dto.ProjectTaskDTO;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Mapper
 public interface ProjectBoardMapper {
-    public boolean insertOrUpdate(String title, String content, String userid, int projectId, MultipartFile file);
     String getTaskCount(String userid, int projectId);
     int updateTask(String title, String content, String userid, int projectId, FileDTO taskFile, String category);
     int insertTask(String title, String content, String userid, String name, int projectId, FileDTO taskFile, String category);

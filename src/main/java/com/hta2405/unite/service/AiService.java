56b224e4411b04ddf9fac42198bb5e2916d5fb91
DNGHKM <kdh940@gmail.com>
2025-01-15 17:39:40 +0900

    [FIX] prompt 파일 누락으로 인한 의존성 제거

--- src/main/java/com/hta2405/unite/service/AiService.java
+++ src/main/java/com/hta2405/unite/service/AiService.java
@@ -19,14 +19,16 @@ public class AiService {
         String template = """ 
                 1. AiScheduleResponseDTO 객체의 구성은 다음과 같습니다.
                 String scheduleName, String scheduleContent, LocalDateTime scheduleStart, LocalDateTime scheduleEnd
-                2. 오늘 날짜는 다음과 같습니다. :""" + LocalDate.now() +
-                """
-                        3. 일정 내용(scheduleContent)은 전체문장을 작성하기보단, 키워드 중심으로 작성하세요
-                        4. 몇시에 끝나는지, 시간이 얼마나 걸리는지 언급이 없으면 1시간을 기본으로 합니다.
-                        5. 다음 입력을 받고 추출해서 AiScheduleResponseDTO로 객체화 해서 반환하세요 : {message}
-                        """;
+                2. 오늘 날짜는 다음과 같습니다. : {today}
+                3. 일정 내용(scheduleContent)은 전체문장을 작성하기보단, 키워드 중심으로 작성하세요
+                4. 몇시에 끝나는지, 시간이 얼마나 걸리는지 언급이 없으면 1시간을 기본으로 합니다.
+                5. 다음 입력을 받고 추출해서 AiScheduleResponseDTO로 객체화 해서 반환하세요 : {message}
+                """;
+
         AiScheduleResponseDTO aiScheduleResponseDTO = chatClient.prompt()
-                .user(promptUserSpec -> promptUserSpec.text(template).param("message", message))
+                .user(promptUserSpec -> promptUserSpec.text(template)
+                        .param("today", LocalDate.now())
+                        .param("message", message))
                 .call()
                 .entity(AiScheduleResponseDTO.class);
 

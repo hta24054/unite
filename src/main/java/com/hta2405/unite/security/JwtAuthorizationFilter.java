8483259a32a01d951080c87a86179f14ee74d488
DNGHKM <kdh940@gmail.com>
2025-01-15 14:32:39 +0900

    [UPDATE] 권한 설정 수정

--- src/main/java/com/hta2405/unite/security/JwtAuthorizationFilter.java
+++ src/main/java/com/hta2405/unite/security/JwtAuthorizationFilter.java
@@ -10,13 +10,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -53,7 +54,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                             .empId(empId)
                             .deptId(deptId)
                             .password("")
-                            .authorities(Collections.singleton(() -> role))
+                            .authorities(List.of(new SimpleGrantedAuthority(role))) // 권한 설정
                             .build();
 
                     UsernamePasswordAuthenticationToken authentication =

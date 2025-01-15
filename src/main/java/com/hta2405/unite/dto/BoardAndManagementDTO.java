a8c3f74d42325c4cf15b56cc56b4a6cd1cf6268b
HuYeoun <kangyh5560@naver.com>
2025-01-15 12:06:31 +0900

    게시판 추가, 수정

---
+++ src/main/java/com/hta2405/unite/dto/BoardAndManagementDTO.java
@@ -0,0 +1,14 @@
+package com.hta2405.unite.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BoardAndManagementDTO {
+    private String boardName1;
+    private String boardName2;
+    private String boardManager;
+}
---
+++ src/main/java/com/hta2405/unite/dto/BoardRequestDTO.java
@@ -0,0 +1,18 @@
+package com.hta2405.unite.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class BoardRequestDTO {
+    private String boardName1;
+    private String OriginalBoardName1;
+    private String boardName2;
+    private String OriginalBoardName2;
+    private List<String> managerId;
+}

a3f0ca6eac47d9b48773b72e25b09a59752eb3ba
choijiiyoung <blueberryxchoco@gmail.com>
2025-01-16 15:12:17 +0900

    자원예약 - 시작 일시와 종료 일시 동일한 경우 얼럿 추가, 얼럿 문구, 팝업 텍스트 수정

--- src/main/resources/static/js/reservation.js
+++ src/main/resources/static/js/reservation.js
@@ -214,15 +214,15 @@ $(document).ready(function () {
                     let _html = "<ul class='deatail_list'>" +
                         "<li>분류명: " + data.resourceType + "</li>" +
                         "<li>자원명: " + data.resourceName + "</li>" +
-                        "<li>시작시간: " + moment(event.start).format("YYYY-MM-DD HH:mm") + "</li>";
+                        "<li>시작 일시: " + moment(event.start).format("YYYY-MM-DD HH:mm") + "</li>";
 
-                    // 종료시간 설정 (시작시간과 동일한 경우)
-                    _html += "<li>종료시간: " +
-                        moment(event.end && !moment(event.start).isSame(event.end) ? event.end : event.start).format("YYYY-MM-DD HH:mm") +
-                        "</li>";
+                    // 종료 일시 설정 (시작 일시와 동일한 경우)
+                    _html += "<li>종료 일시: " +
+                                moment(event.end && !moment(event.start).isSame(event.end) ? event.end : event.start).format("YYYY-MM-DD HH:mm") +
+                             "</li>";
 
                     _html += "<li>예약자: " + data.ename + "</li>" +
-                        "<li>사용용도: " + data.reservationInfo || "" + "</li>";
+                             "<li>사용용도: " + data.reservationInfo || "" + "</li>";
 
                     // 자원정보 존재할 경우
                     if (data.resourceInfo) {
@@ -343,20 +343,26 @@ $(document).ready(function () {
         const $resourceName = $("#resourceName");
 
         if ($start.val().trim() === "") {
-            alert("시작 날짜/시간을 선택하세요");
+            alert("시작 일시를 선택하세요");
             $start.focus();
             return false;
         }
 
         if ($end.val().trim() === "") {
-            alert("종료 날짜/시간을 선택하세요");
+            alert("종료 일시를 선택하세요");
             $end.focus();
             return false;
         }
 
         if (new Date($start.val()) > new Date($end.val())) {
-            alert("끝나는 날짜/시간이 시작 날짜/시간보다 이전입니다. 다시 확인해 주세요.");
+            alert("종료 일시가 시작 시작 일시보다 이전입니다. 다시 확인해 주세요.");
             $start.focus();
+            return false;
+        }
+
+        if ($start.val() === $end.val()) {
+            alert("시작 일시와 종료 일시는 동일할 수 없습니다. 다시 확인해 주세요.");
+            $end.focus();
             return false;
         }
 

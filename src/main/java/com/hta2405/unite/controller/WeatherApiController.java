dc5c4609731e6a74cfa9acfd9c9cd21853c46f44
DNGHKM <kdh940@gmail.com>
2025-01-16 11:17:11 +0900

    [UPDATE] RestController 이름 변경

--- src/main/java/com/hta2405/unite/controller/WeatherApiController.java
+++ src/main/java/com/hta2405/unite/controller/WeatherApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/weather")
 @RequiredArgsConstructor
-public class WeatherController {
+public class WeatherApiController {
     private final WeatherService weatherService;
 
     @GetMapping

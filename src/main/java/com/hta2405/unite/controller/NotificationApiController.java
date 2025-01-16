fe6bbb359327251ddf31ee33b685e5ea07a50f27
DNGHKM <kdh940@gmail.com>
2025-01-16 11:16:48 +0900

    [UPDATE] RestController 이름 변경

--- src/main/java/com/hta2405/unite/controller/NotificationApiController.java
+++ src/main/java/com/hta2405/unite/controller/NotificationApiController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/notification")
-public class NotificationController {
+public class NotificationApiController {
     private final NotificationService notificationService;
 
     @PostMapping

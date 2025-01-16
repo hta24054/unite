d22cb4ec8d2a00be8d5801777feba4ba04f4955c
DNGHKM <kdh940@gmail.com>
2025-01-16 17:01:29 +0900

    [ADD] AI 연락처 조회, 텍스트 요약 기능 추가

--- src/main/java/com/hta2405/unite/controller/api/AiController.java
+++ src/main/java/com/hta2405/unite/controller/api/AiController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -19,17 +18,8 @@ public class AiController {
     private final AiService aiService;
 
     @PostMapping("/chat")
-    public Map<String, Object> chatSchedule(@RequestBody Map<String, String> request, @AuthenticationPrincipal UserDetails user) {
+    public Map<String, Object> chat(@RequestBody Map<String, String> request, @AuthenticationPrincipal UserDetails user) {
         String message = request.get("message");
-        Map<String, Object> map = new HashMap<>();
-        boolean result = aiService.addSchedule(message, user.getUsername());
-        map.put("result", result);
-        String resultMessage = "일정을 추가했습니다.";
-
-        if (!result) {
-            resultMessage = "일정 추가 실패";
-        }
-        map.put("resultMessage", resultMessage);
-        return map;
+        return aiService.findService(message, user.getUsername());
     }
 }

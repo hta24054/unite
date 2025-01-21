5d39db98317424a5b26612180a25382e235d47e4
dontotl18 <dbsgksthf101@naver.com>
2025-01-21 12:52:58 +0900

    detail 배경, text 색상 적용

--- src/main/resources/static/js/project_detail.js
+++ src/main/resources/static/js/project_detail.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     let currentProjectId, memberId;
-
+	color();
     // 업무 내용 클릭 시
     $(".task-content.clickable").click(function () {
         console.log('c');
@@ -287,5 +287,30 @@ $(document).ready(function () {
 		});
 	}
 
-
+	function color(){
+		const projectId = $("#project-id").val();
+		const memberId = $("#user-id").val();
+		$.ajax({
+			url: "/api/project/color",
+			type: "GET",
+			data: {
+				projectId: projectId,
+				memberId: memberId
+			},
+			success: function(data) {
+				console.log("투두 리스트 데이터:");
+				if(data.bgColor == 'rgb(255,255,255)' || data.bgColor == '#ffffff') updateColor('rgb(248,249,250)', data.textColor);
+				else updateColor(data.bgColor, data.textColor);
+			},
+			error: function(xhr, status, error) {
+				console.log("ssdfsdfsdf");
+				console.error("AJAX 오류 발생:", status, error);
+				alert("오류가 발생했습니다. 다시 시도해 주세요.");
+			}
+		});
+	}
+	function updateColor(bgColor, textColor) {
+		$("#background").css('background-color', bgColor);
+		$("#text").css('color', textColor);
+	}
 });

b3cccdcdeb2c8ec5eb5455fc5d70fc0b3e880df7
DNGHKM <kdh940@gmail.com>
2025-01-21 15:22:17 +0900

    [UPDATE] NotificationCategory에 아이콘 정보 추가

--- src/main/java/com/hta2405/unite/enums/NotificationCategory.java
+++ src/main/java/com/hta2405/unite/enums/NotificationCategory.java
@@ -1,19 +1,33 @@
 package com.hta2405.unite.enums;
 
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @RequiredArgsConstructor
 public enum NotificationCategory {
-    DOC("전자문서", "doc"),
-    PROJECT("프로젝트", "project"),
-    SCHEDULE("스케줄", "schedule"),
-    BOARD("게시판", "board");
+    DOC("전자문서", "doc", "fas fa-book-open"),
+    PROJECT("프로젝트", "project", "fa-solid fa-flag"),
+    SCHEDULE("스케줄", "schedule", "fa-solid fa-calendar-days"),
+    BOARD("게시판", "board", "fa-solid fa-comments");
 
     private final String typeKor;
     private final String typeEng;
+    private final String icon;
+
+    @JsonValue
+    public Map<String, String> toJson() {
+        Map<String, String> json = new HashMap<>();
+        json.put("typeKor", typeKor);
+        json.put("typeEng", typeEng);
+        json.put("icon", icon);
+        return json;
+    }
 
     public static NotificationCategory fromString(String type) {
         for (NotificationCategory category : NotificationCategory.values()) {

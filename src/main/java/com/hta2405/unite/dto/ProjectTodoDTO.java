c6ac2c612e0c64c407aa457ecb49c686870a257d
dontotl18 <dbsgksthf101@naver.com>
2025-01-23 15:07:36 +0900

    todo 우선순위

--- src/main/java/com/hta2405/unite/dto/ProjectTodoDTO.java
+++ src/main/java/com/hta2405/unite/dto/ProjectTodoDTO.java
@@ -1,12 +1,9 @@
 package com.hta2405.unite.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
-//@Setter
+@Setter
 @AllArgsConstructor
 @ToString
 @Builder
@@ -14,4 +11,5 @@ public class ProjectTodoDTO {
     private int todoId;
     private String todoSubject;
     private int progressRate;
+    private int orderColumn;
 }

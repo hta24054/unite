f20a1c12a7ec5d84ff7946dba87bb67fb9a9c265
dontotl18 <dbsgksthf101@naver.com>
2025-02-12 15:59:46 +0900

    redis test

--- src/main/java/com/hta2405/unite/service/BirthdayService.java
+++ src/main/java/com/hta2405/unite/service/BirthdayService.java
@@ -25,16 +25,11 @@ public class BirthdayService {
     private final String ServiceKey = "&ServiceKey=";
     private final String apiKey;
     private final EmpMapper empMapper;
-    private final RedisTemplate<String, String> redisTemplate;
     private static final String REDIS_KEY_PREFIX = "birthday:";
-//    private final ObjectMapper objectMapper = new ObjectMapper().configure(SerializationFeature.WRITE_NULL_MAP_VALUES, true);
-    private final ObjectMapper objectMapper;
 
-    public BirthdayService(@Value("${birthday.apiKey}") String apiKey, EmpMapper empMapper, RedisTemplate<String, String> redisTemplate, ObjectMapper objectMapper) {
+    public BirthdayService(@Value("${birthday.apiKey}") String apiKey, EmpMapper empMapper) {
         this.apiKey = apiKey;
-        this.objectMapper = objectMapper;
         this.empMapper = empMapper;
-        this.redisTemplate = redisTemplate;
     }
 
     public BirthdayDTO getLunarDate(LocalDate date) {
@@ -42,35 +37,17 @@ public class BirthdayService {
         String year = formattedDate.substring(0, 4);
         String month = formattedDate.substring(4, 6);
         String day = formattedDate.substring(6, 8);
-
         URI uri = URI.create(BaseURL + "?solYear=" + year + "&solMonth=" + month + "&solDay=" + day + ServiceKey + apiKey + "&_type=json");
-
         RestTemplate restTemplate = new RestTemplate();
-
         return restTemplate.getForObject(uri, BirthdayDTO.class);
     }
 
     public List<Birthday> getTodayBirthdays() {
         LocalDate today = LocalDate.now();
-        String redisKey = REDIS_KEY_PREFIX + today;
-
         BirthdayDTO lunar = getLunarDate(today);
         int lunarMonth = lunar.getResponse().getBody().getItems().getItem().getLunarMonth();
         int lunarDay = lunar.getResponse().getBody().getItems().getItem().getLunarDay();
 
-        try {
-            log.info("redis=========");
-            String cachedBirthdays = redisTemplate.opsForValue().get(redisKey);
-            if (cachedBirthdays != null) {
-                log.info("Redis Cache hit : {}", redisKey);
-                return objectMapper.readValue(cachedBirthdays, new TypeReference<>(){});
-            }
-            List<Birthday> birthdays = empMapper.findTodayBirthdays(today.getMonthValue(), today.getDayOfMonth(), lunarMonth, lunarDay);
-            redisTemplate.opsForValue().set(redisKey, objectMapper.writeValueAsString(birthdays), 24, TimeUnit.HOURS);
-            return birthdays;
-        } catch (Exception e) {
-            log.info(e.getMessage());
-            return empMapper.findTodayBirthdays(today.getMonthValue(), today.getDayOfMonth(), lunarMonth, lunarDay);
-        }
+        return empMapper.findTodayBirthdays(today.getMonthValue(), today.getDayOfMonth(), lunarMonth, lunarDay);
     }
 }
